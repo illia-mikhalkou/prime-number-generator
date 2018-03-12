c53b7c79d84c9b38305a50939505d7a9d96668cd
Illia Mikhalkou <illia.mikhalkou@gmail.com>
2018-03-12 14:45:06 +0300

    UI validation is added.

--- PrimeNumberGenerator/PrimeNumberGenerator.WebApi/Scripts/app/controllers/prime-number-generator-controller.js
+++ PrimeNumberGenerator/PrimeNumberGenerator.WebApi/Scripts/app/controllers/prime-number-generator-controller.js
@@ -1,7 +1,7 @@
 ﻿angular
 	.module('PrimeNumberGeneratorApp', [])
 	.controller('PrimeNumberGeneratorCtrl', ['$scope', '$http', function ($scope, $http) {
-		$scope.inputNumber = 1;
+		$scope.inputNumber = undefined;
 		$scope.results = [];
 
 		$scope.checkNumber = function () {
