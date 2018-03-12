b78ffe6c11a825c4699e6d4617bc6403c0cfbb23
Illia Mikhalkou <ilya1992m@gmail.com>
2018-03-12 22:13:55 +0300

    Added unit tests for prime number service.

---
+++ PrimeNumberGenerator/PrimeNumberGenerator.Tests/PrimeNumberServiceTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PrimeNumberGenerator.WebApi.Interfaces;
+using PrimeNumberGenerator.WebApi.Services.PrimeNumber;
+using System;
+
+namespace PrimeNumberGenerator.Tests
+{
+	[TestClass]
+	public class PrimeNumberServiceTests
+	{
+		private readonly IPrimeNumberService primeNumberService;
+
+		public PrimeNumberServiceTests()
+		{
+			this.primeNumberService = new PrimeNumberService();
+		}
+
+		[TestMethod]
+		public void TestPrimeNumbers()
+		{
+			Assert.IsTrue(this.primeNumberService.IsPrime(2));
+			Assert.IsTrue(this.primeNumberService.IsPrime(5));
+			Assert.IsTrue(this.primeNumberService.IsPrime(149));
+			Assert.IsTrue(this.primeNumberService.IsPrime(11777));
+			Assert.IsTrue(this.primeNumberService.IsPrime(25747));
+			Assert.IsTrue(this.primeNumberService.IsPrime(99991));
+		}
+
+		[TestMethod]
+		public void TestNonPrimeNumbers()
+		{
+			Assert.IsFalse(this.primeNumberService.IsPrime(4));
+			Assert.IsFalse(this.primeNumberService.IsPrime(25));
+			Assert.IsFalse(this.primeNumberService.IsPrime(169));
+			Assert.IsFalse(this.primeNumberService.IsPrime(18045));
+			Assert.IsFalse(this.primeNumberService.IsPrime(25749));
+		}
+
+		[TestMethod]
+		public void TestExceptions()
+		{
+			Assert.ThrowsException<ArgumentException>(() => this.primeNumberService.IsPrime(-1));
+			Assert.ThrowsException<ArgumentException>(() => this.primeNumberService.IsPrime(int.MinValue));
+			Assert.ThrowsException<ArgumentException>(() => this.primeNumberService.IsPrime(0));
+		}
+	}
+}
---
+++ PrimeNumberGenerator/PrimeNumberGenerator.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PrimeNumberGenerator.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PrimeNumberGenerator.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0ae44d83-3c1c-43cd-92dd-3acb8ad41ba2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
