42636c4f242a37b9afb23cf87c202957acd0296b
Illia Mikhalkou <illia.mikhalkou@gmail.com>
2018-03-12 14:13:05 +0300

    UI implemented

--- PrimeNumberGenerator/PrimeNumberGenerator.WebApi/Scripts/app/controllers/prime-number-generator-controller.js
+++ PrimeNumberGenerator/PrimeNumberGenerator.WebApi/Scripts/app/controllers/prime-number-generator-controller.js
@@ -1,5 +1,10 @@
 ﻿angular
 	.module('PrimeNumberGeneratorApp', [])
 	.controller('PrimeNumberGeneratorCtrl', ['$scope', '$http', function ($scope, $http) {
+		$scope.inputNumber = 1;
+		$scope.results = [];
 
+		$scope.checkNumber = function () {
+			console.log('checking...');
+		}
 	}]);
